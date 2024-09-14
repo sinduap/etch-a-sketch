e82f89b592fc5a864c1d7c6d9d3800fd3d2bd02c
Sindu Andita Pratama <sindu.andita@gmail.com>
2024-09-14 16:38:41 +0700

    Add feature grid pointer

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const DEFAULT_GRID_SIZE = 16;
 const DEFAULT_COLOR = '#3a3a3a';
-const COLOR_WHITE = '#fff';
+const COLOR_WHITE = '#ffffff';
 const DEFAULT_MODE = 'DEFAULT';
 const RAINBOW_MODE = 'RAINBOW';
 const ERASE_MODE = 'ERASE';
@@ -17,6 +17,7 @@ let isActive = false;
 let mode = DEFAULT_MODE;
 let activeColor = DEFAULT_COLOR;
 let currentGridSize = DEFAULT_GRID_SIZE;
+let tempColor;
 
 window.addEventListener('mousedown', function () {
   isActive = true;
@@ -32,7 +33,7 @@ window.addEventListener('load', function () {
 });
 
 createGridBtn.addEventListener('click', function () {
-  const gridCount = +prompt('Create how many grid?', DEFAULT_GRID_SIZE);
+  const gridCount = +prompt('Enter grid cells', DEFAULT_GRID_SIZE);
   if (!gridCount) return;
   currentGridSize = gridCount;
   if (!Number.isInteger(gridCount) || gridCount < 1) {
@@ -79,29 +80,58 @@ eraseBtn.addEventListener('click', function () {
 });
 
 clearBtn.addEventListener('click', () => {
-  const answer = confirm('Are you sure? (Press Esc to cancel)');
+  const answer = confirm('Are you sure to delete this sketch?');
   if (!answer) return;
   grid.innerHTML = '';
   createGrid(currentGridSize);
 });
 
-function handleSketch(e) {
-  if (e.type === 'mouseover' && !isActive) return;
+function handleSketch(event) {
+  const { target, type } = event;
+  if (type === 'mouseover' && !isActive) {
+    tempColor = target.style.backgroundColor;
+    switch (mode) {
+      case DEFAULT_MODE:
+      case ERASE_MODE:
+        target.style.backgroundColor = opaquedHex(activeColor);
+        break;
+      case RAINBOW_MODE:
+        target.style.backgroundColor = createRandomColor(0.5);
+        break;
+    }
+    return;
+  }
+
+  if (type === 'mouseleave' && !isActive) {
+    target.style.backgroundColor = tempColor;
+    return;
+  }
+
+  if (type === 'mouseup') {
+    if (mode === ERASE_MODE) {
+      tempColor = COLOR_WHITE;
+      return;
+    }
+    tempColor = activeColor;
+    return;
+  }
+
   switch (mode) {
     case DEFAULT_MODE:
-      e.target.style.backgroundColor = activeColor;
+      target.style.backgroundColor = activeColor;
       break;
     case ERASE_MODE:
-      e.target.style.backgroundColor = COLOR_WHITE;
+      target.style.backgroundColor = COLOR_WHITE;
       break;
     case RAINBOW_MODE:
-      e.target.style.backgroundColor = createRandomColor();
+      target.style.backgroundColor = createRandomColor();
       break;
   }
 }
 
 function createGrid(gridSize = DEFAULT_GRID_SIZE) {
   const cell = document.createElement('div');
+  cell.style.backgroundColor = COLOR_WHITE;
   cell.classList.add('cell');
   cell.style.height = `${grid.clientHeight / gridSize}px`;
   cell.style.width = `${grid.clientWidth / gridSize}px`;
@@ -109,7 +139,9 @@ function createGrid(gridSize = DEFAULT_GRID_SIZE) {
   for (let i = 0; i < gridSize * gridSize; i++) {
     const child = cell.cloneNode();
     child.addEventListener('mouseover', handleSketch);
+    child.addEventListener('mouseleave', handleSketch);
     child.addEventListener('mousedown', handleSketch);
+    child.addEventListener('mouseup', handleSketch);
     grid.append(child);
   }
 }
@@ -118,8 +150,28 @@ function createGrid(gridSize = DEFAULT_GRID_SIZE) {
 const randomBetween = (min, max) =>
   min + Math.floor(Math.random() * (max - min + 1));
 
-const createRandomColor = () =>
-  `rgb(${randomBetween(0, 255)},${randomBetween(0, 255)},${randomBetween(
-    0,
-    255
-  )})`;
+const createRandomColor = alpha =>
+  alpha
+    ? `rgba(${randomBetween(0, 255)},${randomBetween(0, 255)},${randomBetween(
+        0,
+        255
+      )}, ${alpha})`
+    : `rgb(${randomBetween(0, 255)},${randomBetween(0, 255)},${randomBetween(
+        0,
+        255
+      )})`;
+
+const opaquedHex = (hex, alpha = 0.5) => {
+  if (/^#([A-Fa-f0-9]{3}){1,2}$/.test(hex)) {
+    let color = hex.substring(1).split('');
+    if (color.length == 3) {
+      color = [color[0], color[0], color[1], color[1], color[2], color[2]];
+    }
+    color = '0x' + color.join('');
+    const rgb = [(color >> 16) & 255, (color >> 8) & 255, color & 255].join(
+      ','
+    );
+    return `rgba(${rgb},${alpha})`;
+  }
+  throw new Error('Bad Hex');
+};
